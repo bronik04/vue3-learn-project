feb9ccfae024a5fbfd78d4eb87f56361aaa35694
Nikolay Brovkin <bronik004@gmail.com>
2023-01-10 10:23:05 +0300

    feat: added style and post

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    Hello Vue
+    <div class="post">
+      <div><strong>Название:</strong> Пост о JavaScript</div>
+      <div><strong>Описание:</strong> JavaScript язык программирования</div>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
 
+    }
+  },
+  methods: {
+  }
+}
 </script>
 
 <style>
-
+ * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+ }
 </style>
